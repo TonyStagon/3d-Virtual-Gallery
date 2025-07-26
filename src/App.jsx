f00beeb62898ad6c7667def0f02d40f184abd1d3
Tony Stagon <leelethabi@gmail.com>
2025-07-26 18:44:26 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, useTexture } from "@react-three/drei";
+import * as THREE from "three";
+
+// Wall component
+function Wall({ position, rotation, size, image }) {
+  const texture = useTexture(image);
+  return (
+    <mesh position={position} rotation={rotation}>
+      <planeGeometry args={size} />
+      <meshStandardMaterial map={texture} side={THREE.DoubleSide} />
+    </mesh>
+  );
+}
+
+// Floor component
+function Floor() {
+  return (
+    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -1, 0]}>
+      <planeGeometry args={[30, 30]} />
+      <meshStandardMaterial color="#999" />
+    </mesh>
+  );
+}
+
+// Museum scene
+function Museum() {
+  return (
+    <>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[10, 10, 5]} intensity={1} />
+      <Floor />
+
+      {/* Left wall */}
+      <Wall
+        position={[-5, 2, 0]}
+        rotation={[0, Math.PI / 2, 0]}
+        size={[4, 4]}
+        image="/src/assets/fox art.png"
+      />
+
+      {/* Right wall */}
+      <Wall
+        position={[5, 2, 0]}
+        rotation={[0, -Math.PI / 2, 0]}
+        size={[4, 4]}
+        image="/src/assets/smile.png"
+      />
+
+      {/* Back wall */}
+      <Wall
+        position={[0, 2, -5]}
+        rotation={[0, 0, 0]}
+        size={[4, 4]}
+        image="/src/assets/art girl.png"
+      />
+
+      <OrbitControls />
+    </>
+  );
+}
+
+export default function App() {
+  return (
+    <Canvas camera={{ position: [0, 2, 10], fov: 50 }}>
+      <Museum />
+    </Canvas>
+  );
+}
---
+++ src/components/GalleryRoom.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { OrbitControls, useTexture } from "@react-three/drei";
+import * as THREE from "three";
+
+// Wall component with an image
+function Wall({ position, rotation, size, image }) {
+  const texture = useTexture(image);
+  return (
+    <mesh position={position} rotation={rotation}>
+      <planeGeometry args={size} />
+      <meshStandardMaterial map={texture} side={THREE.DoubleSide} />
+    </mesh>
+  );
+}
+
+// Floor
+function Floor() {
+  return (
+    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -1, 0]}>
+      <planeGeometry args={[30, 30]} />
+      <meshStandardMaterial color="#999" />
+    </mesh>
+  );
+}
+
+export default function GalleryRoom() {
+  return (
+    <>
+      <ambientLight intensity={0.4} />
+      <directionalLight position={[5, 10, 7]} intensity={0.8} />
+      <Floor />
+
+      {/* Grid layout with clear tiling effect */}
+      {[0, 1, 2, 3, 4].map((col) => (
+        <group key={`col-${col}`}>
+          {/* Front row */}
+          <Wall
+            position={[-8 + col * 4, 1.2, -8]}
+            rotation={[0, 0, 0]}
+            size={[3.5, 3.5]}
+            image={`/src/assets/art${(col + 1) % 3 + 1}.png`}
+          />
+          {/* Middle row */}
+          <Wall
+            position={[-8 + col * 4, 1.2, -4]}
+            rotation={[0, 0, 0]}
+            size={[3.5, 3.5]}
+            image={`/src/assets/art${(col + 2) % 3 + 1}.png`}
+          />
+          {/* Top row */}
+          <Wall
+            position={[-8 + col * 4, 4.5, -4]}
+            rotation={[0, 0, 0]}
+            size={[3.5, 3.5]}
+            image={`/src/assets/art${col % 3 + 1}.png`}
+          />
+        </group>
+      ))}
+
+      {/* Camera Controls */}
+      <OrbitControls />
+    </>
+  );
+}
